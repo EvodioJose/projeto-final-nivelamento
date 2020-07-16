de04c4a29de36de17f826bc53e275a776bfe2939
Evódio Aquino <evodiojaquino@gmail.com>
2020-07-16 11:28:14 -0300

    Teste Final

---
+++ projeto-final/javaScript.js
@@ -0,0 +1,38 @@
+var carro = ("fiat, vokswagem"); // variavel recebe e armazena dados tipo texto, numeros,
+const endereço = ("R. Bananeiras, 50"); // recebe um valor fixo que não tem alteração
+if (5>2) // satisfeita essa condição retorna verdade
+else (5>10)  // retorna falso
+
+
+//loops tipos: while; busca em um bloco de codigo até que uma condição seja satisfeita. ex: (1< 10) {numero = "um numero" +1; 1++}
+            /* for  busca em um bloco de codigo até que a condição seja satisfeita exp: var numeros =  1,7,8,9, for (numeros)
+                for/in busca através das propriedades do objeto 
+                for/off busca através das propriedades rertornando a posição d
+                escopos: globais e locais ( o codigo no escopo local não pode ser chamado pelo globalThis, e ao contrário isso é posssivél)
+                output: document.write; console log; get.elemnt.ById; serve para saidas de dados 
+                imput: alert; innerhtml; prompt serve para entradas de dados
+                array = coleção  var nomes = [ Evodio, lucas, josé ] 
+                objeto = var = tv { marca: LG, modelo: lg3256, tamnho: 55"}
+                cameCase = primeira palavra começa com minuscula e a segunda começa com maiuscula.
+                PascalCase = as duas palavras começam com maiuscula
+                kibab-case =
+                Snake_case = primeira palavra e sgunda com letras em lawercase separando uma da outra po wanderline
+                Caminho Absoluto vai até uma pasta buscar um arquivo.
+                Caminha Relativo vai direto até o arquivo.
+                E = Entender : ou seja para que eu faço algo preciso primeiro saber do que se trata.
+                P = Projetar ou programar : já entendi agora tenho que saber como vou fazer.
+                E = Executar : já sei o que é, já sei como fazer, agora é fazer.
+                R = Revisar : Concebi á ideia, planejei, executei, será que deu certo, tenho que verificar.
+                PiLha = forma de ordenar um dados um em cima do outro, detalhe em uma pilha o ultimo a entrar e o primeiro a sair; se ultiliza quando os dados podem ser inseridos ou retirados sem uma ordem precisa pois quando se inserem ou remove o ultimo da fila
+                Lista = forma de ordenamento de dados que possibilita a entrada e saida de dados em posições pré determinadas por comandos especificos
+
+
+                html = è uma linguagem que da á estrutura e forma que uma webpg vai funcionar
+                CSS = è roupagem ou seja dá o acabamento nessa estrutura para que ela se torne visivélmente agradavél
+                java Script = Faz a interação fazendo o funcionamento de todo o processo dando movimento ao processo.
+
+
+    
+
+              
+                  }
